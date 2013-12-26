11e0608c1275ce08f234ebd07d0974146e2a37c1
fire <84426590@qq.com>
2013-12-26 16:06:57 +0800

    增加 horizontalListView

--- src/com/linkin/app/LinkinApplication.java
+++ src/com/linkin/app/LinkinApplication.java
@@ -2,6 +2,27 @@ package com.linkin.app;
 
 import android.app.Application;
 
-public class LinkinApplication extends Application{
+import java.util.HashMap;
+import java.util.Map;
 
+public class LinkinApplication extends Application{
+    private Map<String, Object> paramMap;
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        paramMap = new HashMap<String, Object>();
+    }
+    
+    public void setParam(String key, Object obj) {
+        paramMap.put(key, obj);
+    }
+
+    public Object getParam(String key) {
+        return paramMap.get(key);
+    }
+    
+    public boolean hasParam(String key){
+        return paramMap.containsKey(key);
+    }
 }
